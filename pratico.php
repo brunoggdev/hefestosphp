3f69de2573ed4fb3daaf47888aa6a0e733068749
brunoggdev <brunoggdev@gmail.com>
2023-03-04 19:39:37 -0300

    Criado comando fazer do CLI

--- pratico.php
+++
@@ -1,3 +0,0 @@
-<?php
- require 'system/CLI/cli.php';
-?>
--- system/CLI/cli.php
+++ system/CLI/cli.php
@@ -1,21 +1,65 @@
 <?php
 
-if (isset($argc)) {
-
-    match ($argv[1]) {
-        'serve' => exec('php -S localhost:8080 -t public'),
-        'fazer' => fazer($argv[2])
-    };
+match ($argv[1]) {
+    'serve', 'servir', 'iniciar' => serve($argv[2] ?? '8080'),
+    'fazer' => fazer( ucfirst($argv[2]), $argv[3] ?? '')
+};
     
+
+/**
+* Inicia um servidor embutido do PHP para a pasta public 
+* na porta desejada (padrão 8080)
+* @author Brunoggdev
+*/
+function serve($porta)
+{
+    exec("php -S localhost:$porta -t public");
 }
+
 
 
 /**
 * Cria um novo arquivo com as propriedades desejadas
 * @author Brunoggdev
 */
-function fazer($arquivo)
+function fazer($arquivo, $nome)
 {
-    // TODO: Gerar o arquivo desejado.
-    echo $arquivo;
+
+    if(empty($nome)){
+        echo "\n # Você deve informar um nome pro arquivo depois do tipo.\n";
+        echo "\n # Ex.: php pratico fazer $arquivo Usuarios$arquivo.\n\n";
+        exit;
+    }
+
+
+    require 'app/Config/constantes.php';
+
+    $caminho = match ($arquivo) {
+        'Controller' =>  BASE_PATH . 'app/Controllers/',
+        'Model' => BASE_PATH . 'app/Models/',
+    };
+
+    $base = $arquivo . 'Base';
+    $namespace = 'App\\' . $arquivo . 's';
+    
+    $template = <<<EOT
+    <?php
+
+    namespace $namespace;
+
+    class $nome extends $base
+    {
+        // Seus metodos de controller aqui
+    }
+
+    EOT;
+
+    // Replace the template placeholders with actual values
+    // $template = str_replace('{{class_name}}', $nomeDoArquivo, $template);
+
+    if ( file_put_contents("$caminho$nome.php", $template) ) {
+        echo "$arquivo criado sucesso.\n";
+    } else {
+        echo "Algo deu errado ao gerar o $arquivo.\n";
+    }
 }
