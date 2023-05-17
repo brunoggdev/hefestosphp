c6d82593339135a0d42f4fabec49554daf11cfbf
Bruno Gomes <brunoggdev@gmail.com>
2023-05-17 15:30:39 -0300

    modelo inicial limpo

--- app/Auxiliares.php
+++ app/Auxiliares.php
@@ -2,8 +2,7 @@
 # ----------------------------------------------------------------------
 # Arquivo de funções auxiliares do usuário do HefestosPHP.
 # Aqui você pode criar suas próprias funções auxiliares e 
-# elas ficarão disponíveis em toda sua aplicação 
-# pois este arquivo é carregado no inicio  da execução.
+# elas ficarão disponíveis em toda sua aplicação.
 # ----------------------------------------------------------------------
 
 // function exemploPrimeiraFuncao(){
