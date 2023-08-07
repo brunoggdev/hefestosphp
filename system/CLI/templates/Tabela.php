237a58b3345828d9d1d9b9483502c3a12497897a
Bruno Gomes <brunoggdev@gmail.com>
2023-08-07 09:57:15 -0300

    template atualizado

--- system/CLI/templates/Tabela.php
+++ system/CLI/templates/Tabela.php
@@ -6,12 +6,6 @@ return <<<EOT
 
     use Hefestos\Database\Tabela;
 
-    /* ----------------------------------------------------------------------
-    Você deve retornar a sql para criar uma tabela como desejar.
-    Pode ser utilizando a classe Tabela ou mesmo uma string pura.
-    ---------------------------------------------------------------------- */
-
-    
     return ( new Tabela('{nome}') )
         ->id();
     EOT;
