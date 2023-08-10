a3ff3f7481b299bc641dc4c3db5da6e9a54621d6
Bruno Gomes <brunoggdev@gmail.com>
2023-08-10 11:05:42 -0300

    ajuste

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
 /* ----------------------------------------------------------------------
- * Este arquivo faz toda a configuração necessária para a inicialização 
- * do app como buscar as constantes, carregar as funções auxiliares e 
- * registrar o autoloader adequado; Depois retorna uma instância do app.
+ Este arquivo faz toda a configuração necessária para a inicialização 
+ do app como buscar as constantes, carregar as funções auxiliares e 
+ registrar o autoloader adequado;
 ---------------------------------------------------------------------- */
 require '../system/Core/iniciar_app.php';
 
