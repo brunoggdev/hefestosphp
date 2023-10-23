d7f53e1724b643e1e806686fdeb7901c6c554dd7
Bruno Gomes <114432497+brunoggdev@users.noreply.github.com>
2023-10-23 14:09:06 -0300

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -3,7 +3,7 @@
 return [
     'driver'  => 'mysql', // mysql ou sqlite
     'host'    => 'localhost', // caso driver seja mysql
-    'nomeDB'  => 'lojinha', // caso driver seja mysql
+    'nomeDB'  => 'olimpo', // caso driver seja mysql
     'usuario' => 'root',
     'senha'   => '',
     'sqlite'  => 'app/Database/sqlite/default.sqlite', // caso driver seja sqlite
