3ac126dbaefb8ac2d30da2303eb934bde78728a3
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-06-28 02:02:07 -0300

    atualizacao montarPagina

--- app/auxiliares.php
+++ app/auxiliares.php
@@ -8,11 +8,11 @@ que desejar e elas ficarão disponíveis em toda sua aplicação.
  * Carrega a view informada com o header e footer padrão 
  * além das modais e js específicos
 */
-function montarPagina(string $nome_da_view, array $dados):string
+function montarPagina(string $nome_da_view, array $dados = []):string
 {
-    return comp('header', ['titulo' => ucfirst($nome_da_view)])
+    return comp('header', ['titulo' => extrair_item('titulo', $dados)])
         .view($nome_da_view, $dados)
         .componente("modais_$nome_da_view")
-        .importar_js($nome_da_view)
+        .importar_js($nome_da_view, true)
         .comp('footer');
 }
