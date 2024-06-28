e99e789813f5e1ce13e861b6107e01ccbf0f062e
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-06-27 23:52:30 -0300

    auxiliar montarPagina criada

--- app/auxiliares.php
+++ app/auxiliares.php
@@ -4,10 +4,15 @@ Neste aquivo você pode definir suas funções auxiliares com o comportamento
 que desejar e elas ficarão disponíveis em toda sua aplicação.
 ---------------------------------------------------------------------- */
 
-// function exemploPrimeiraFuncao(){
-//     // seu codigo aqui
-// }
-
-// function exemploSegundaFuncao($param){
-//     // seu outro codigo aqui
-// }
+/**
+ * Carrega a view informada com o header e footer padrão 
+ * além das modais e js específicos
+*/
+function montarPagina(string $nome_da_view, array $dados):string
+{
+    return comp('header', ['titulo' => ucfirst($nome_da_view)])
+        .view($nome_da_view, $dados)
+        .componente("modais_$nome_da_view")
+        .importar_js($nome_da_view)
+        .comp('footer');
+}
