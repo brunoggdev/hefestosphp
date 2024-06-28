9435f78a8000eb02eb155507c3efb18c037fbb71
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-06-28 02:04:31 -0300

    rotas demo att

--- app/Config/rotas.php
+++ app/Config/rotas.php
@@ -8,3 +8,14 @@ função anonima ou um metodo de controller. Consulte a documentação.
 $rotas->get('/', function() {
     return view('home');
 });
+
+$rotas->get('/demo', function() {
+    return montarPagina('demo');
+});
+
+$rotas->get('/outra-demo', function() {
+    return json([
+        'cor' => 'success',
+        'texto' => 'Olá do backend!'
+    ]);
+});
