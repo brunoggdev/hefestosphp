ff390ee81324821f51cdaff8a4d8b8096f5adb04
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-09-14 03:57:59 -0300

    uso de sessao no footer atualizado

--- app/Views/componentes/footer.php
+++ app/Views/componentes/footer.php
@@ -1,14 +1,11 @@
 <?=comp('padroes_hefestos')?>
 
 
-<?php
-
-if (sessao()->tem('toast')) {
-    $toast = sessao('toast');
-    echo "<script type=module> novo_toast('$toast[texto]', '$toast[cor]') </script>";
-}
-
-?>
+<?php if(sessao()->tem('toast')): ?>
+<script type="module" >
+    toast('<?= sessao('toast.texto') ?>', '<?= sessao('toast.cor') ?>')
+</script>
+<?php endif; ?>
 
 </body>
 </html>
